e922f7f1e9df888e82d4218c1adabb2d4eef7917
Andrew Potter <hello@drewpotter.com>
2017-11-07 14:59:58 +0000

    Added generation of json filters from database

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,13 @@
 class HomeController < ApplicationController
   def index
-    # dummy data to build out the front end
+
+    projects = Project.all.order(start_date: :desc)
+    unique_donors = projects.uniq{|x| x.donors}.map{ |e| e.donors }
+    unique_start_date = projects.uniq{|x| x.start_date}.map{ |e| e.start_date }
+    unique_end_date = projects.uniq{|x| x.end_date}.map{ |e| e.end_date }
+    unique_country = projects.uniq{|x| x.country}.map{ |e| e.country }
+    unique_ocean_based_region = projects.uniq{|x| x.ocean_based_region}.map{ |e| e.ocean_based_region }
+
     filters = [
       {
         title:"ID"
@@ -11,7 +18,7 @@ class HomeController < ApplicationController
       {
         name: "donor",
         title: "Donor(s)",
-        options: [ "Donor name", "Donor name 2", "Donor name 3" ]
+        options: unique_donors
       },
       {
         name: "status",
@@ -21,84 +28,30 @@ class HomeController < ApplicationController
       {
         name: "startDate",
         title: "Start Date",
-        options: [ '2000', '2003' ]
+        options: unique_start_date
       },
       {
         name: "endDate",
         title: "End Date",
-        options: [ '2002', '2005' ]
+        options: unique_end_date
       },
       {
         name: "country",
         title: "Country",
-        options: [ "UK", "Australia"]
+        options: unique_country
       },
       {
         name: "oceanBasedRegion",
         title: "Ocean Based Region",
-        options: [ "Ocean 2"]
+        options: unique_ocean_based_region
       },
       {
         title: "Total Project Cost"
       }
     ]
 
-    projects = [
-      {
-        id: 0,
-        projectTitle: "Project 1",
-        donor: "Donor name",
-        status: "Active",
-        startDate: 2000,
-        endDate: 2002,
-        country: "UK",
-        oceanBasedRegion: "Ocean",
-        beneficiaries: "Beneficiary name",
-        implementingAgency: "Agency name",
-        totalProjectCost: "£300000",
-        coFundingEntities: "Grant £300",
-        category: "Category name",
-        teamLeader: "Leader name",
-        furtherInformation: "More info goes in this ",
-      },
-      {
-        id: 1,
-        projectTitle: "Project 2",
-        donor: "Donor name 2",
-        status: "Closed",
-        startDate: 2003,
-        endDate: 2005,
-        country: "Australia",
-        oceanBasedRegion: "Ocean",
-        beneficiaries: "Beneficiary name 2",
-        implementingAgency: "Agency name 2",
-        totalProjectCost: "£100000",
-        coFundingEntities: "Grant £100",
-        category: "Category name 2",
-        teamLeader: "Leader name 2",
-        furtherInformation: "More info goes in this 2",
-      },
-      {
-        id: 2,
-        projectTitle: "Project 3",
-        donor: "Donor name 3",
-        status: "Closed",
-        startDate: 2003,
-        endDate: 2005,
-        country: "UK",
-        oceanBasedRegion: "Ocean 2",
-        beneficiaries: "Beneficiary name 3",
-        implementingAgency: "Agency name 3",
-        totalProjectCost: "£111",
-        coFundingEntities: "Grant £111",
-        category: "Category name 3",
-        teamLeader: "Leader name 3",
-        furtherInformation: "More info goes in this 3",
-      }
-    ]
-
     @filters = filters.to_json
-    @projects = Project.all.order(start_date: :desc).to_json
+    @projects = projects.to_json
 
   end
 
