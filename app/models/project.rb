3bd2a0eaadd7a8dddb6b248f499ba0ca0fefaeec
Andrew Potter <hello@drewpotter.com>
2017-11-27 11:56:45 +0000

    Moved the filters and projects json generation into the project model and updated the home_controller

--- app/models/project.rb
+++ app/models/project.rb
@@ -11,6 +11,84 @@ class Project < ApplicationRecord
   has_many :project_ocean_regions
   has_many :ocean_regions, through: :project_ocean_regions
 
+  def self.filters_to_json
+    projects = Project.all.order(id: :asc)
+    unique_donors = Donor.pluck(:name).sort
+    unique_countries = Country.pluck(:name).sort
+    unique_ocean_based_regions = OceanRegion.pluck(:name).sort
+    unique_categories = projects.pluck(:category).uniq.sort
+    unique_ecosystems = Ecosystem.pluck(:name).sort
+
+    filters = [
+      {
+        title:"ID"
+      },
+      {
+        title: "Project Title"
+      },
+      {
+        name: "donors",
+        title: "Donor(s)",
+        options: unique_donors,
+        type: 'multiple'
+      },
+      {
+        name: "status",
+        title: "Status",
+        options: [ "Active", "Closed" ]
+      },
+      {
+        name: "category",
+        title: "Category",
+        options: unique_categories
+      },
+      {
+        name: "ecosystem",
+        title: "Ecosystem",
+        options: unique_ecosystems,
+        type: 'multiple'
+      },
+      {
+        name: "country",
+        title: "Country",
+        options: unique_countries,
+        type: 'multiple'
+      },
+      {
+        name: "ocean_based_region",
+        title: "Ocean Region",
+        options: unique_ocean_based_regions,
+        type: 'multiple'
+      },
+      {
+        title: "Total Project Cost"
+      }
+    ].to_json
+  end
+
+  def self.projects_to_json
+    projects = Project.all.to_a.map! do |project|
+      {
+        id: project.id,
+        project_title: project.project_title,
+        donors: project.donors.pluck(:name).sort,
+        status: project.status,
+        start_date: project.start_date,
+        end_date: project.end_date,
+        country: project.countries.pluck(:name).sort,
+        ecosystem: project.ecosystems.pluck(:name).sort,
+        ocean_based_region: project.ocean_regions.pluck(:name).sort,
+        beneficiaries: project.beneficiaries,
+        implementing_agency: project.implementing_agency,
+        total_project_cost: project.total_project_cost,
+        co_funding_entities: project.co_funding_entities,
+        category: project.category,
+        team_leader: project.team_leader,
+        further_information: project.further_information
+      }
+    end.to_json
+  end
+
   def self.to_csv(ids = [])
 
     csv = ''
