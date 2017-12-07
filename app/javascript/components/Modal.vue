a9dccb2c20d9b7a36c15b9dca922b0f9d1be046f
Stacy Talbot <stacy.talbot@unep-wcmc.org>
2017-12-07 13:20:58 +0000

    Add Primary Funding to the modal

--- app/javascript/components/Modal.vue
+++ app/javascript/components/Modal.vue
@@ -18,6 +18,7 @@
         <p><strong>Beneficiaries:</strong> {{ modalContent.beneficiaries }}</p>
         <p><strong>Implementing Agency:</strong> {{ modalContent.implementing_agency }}</p>
         <p><strong>Total Project Cost:</strong> {{ modalContent.total_project_cost }}</p>
+        <p><strong>Primary Funding:</strong> {{ modalContent.primay_funding }}</p>
         <p><strong>Co-funding:</strong> {{ modalContent.co_funding_entities }}</p>
         <p><strong>Category:</strong> {{ modalContent.category }}</p>
         <p><strong>Further Information:</strong> {{ modalContent.further_information }}</p>
