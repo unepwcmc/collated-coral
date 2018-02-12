e6a81af09615b1a7063a329cc53779a8a038e644
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2018-02-12 17:02:11 +0000

    Add weblink field to projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171207114352) do
+ActiveRecord::Schema.define(version: 20180212165400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 20171207114352) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "primary_funding"
+    t.string "weblink", default: ""
   end
 
 end
