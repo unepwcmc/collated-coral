8d1d77891081ec036aa7c6a497b91b81330ad050
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2018-02-13 10:29:11 +0000

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20180213100008) do
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
-    t.string "CreateCategoryModelAndRelationshipWithProject"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
